f0c16955c3e7e39401267726167c4f99d9805047
Paula <pbarter@upv.edu.es>
2025-10-23 18:37:36 +0200

    Métodos virtuales puros y genéricos creados

--- ListArray.h
+++ ListArray.h
@@ -76,42 +76,91 @@ class ListArray : public List<T> {
 
 	//heredados
             void insert(int pos, T e) override {
+                if (pos < 0 || pos >= n){
+                        throw out_of_range ("Fuera de rango"); 
+                }
 
+                if (n >= max){
+                        resize(max *2); 
+                }
+
+                for(int i = n; i < pos; i--){
+                        arr[i] = arr[i - 1];
+                }
+
+                arr[pos] = e; 
+
+                n++; 
 
             }
-
-            void append(T e) override {
-                    //
-
-            }
-
 
             void prepend(T e) override {
-                    //
+                if (n >= max){
+                        resize(max *2); 
+                }
+                for(int i = n; i < 0; i--){
+                        arr[i] = arr[i - 1];
+                }
+
+                arr[0] = e; 
+
+                n++; 
+            }
+
+
+            void append(T e) override {
+                if (n >= max){
+                        resize(max *2); 
+                }
+                
+                arr[n] = e; 
+
+                n++; 
 
             }
 
 
             T remove(int pos) override {
-                    //
+                if (pos < 0 || pos >= n){
+                        throw out_of_range ("Fuera de rango"); 
+                }
 
+                T element = arr[pos]; 
+
+                for(int i = pos; i < n - 1; i++){
+                        arr[i] = arr[i - 1];
+                }
+
+                n--; 
+
+                return element; 
             }
 
             T get(int pos) override {
-                    //
+                 if (pos < 0 || pos >= n){
+                        throw out_of_range ("Fuera de rango"); 
+                }
+
+                return arr[pos]; 
             }
 
             int search(T e) override {
-
+                for(int i = 0; i < n; i++){
+                        if(arr[i] == e){
+                                return i + 1; 
+                        }
+                }
+                return -1; 
             }
 
             bool empty() override {
-
+                if (n == 0) return true; 
+                else return false; 
             }
 
 
             int size() override {
-
+                return n; 
             }
 
 
