e687fa93deef19ce270df2510dcfb0263e4be0ed
Paula <pbarter@upv.edu.es>
2025-10-23 18:03:56 +0200

    Métodos nuevos, ListArray.h

--- ListArray.h
+++ ListArray.h
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <ostream>
+#include <stdexcept>
 #include "List.h"
 using namespace std; 
 
@@ -12,7 +13,21 @@ class ListArray : public List<T> {
 	    T* arr; 
 	    int max; 
 	    int n; 
-	    static const int MINSIZE; 
+	    static const int MINSIZE = 2; 
+
+            void resize(int new_size){
+		   T* new_arr = new T[new_size]; 
+                   for (int i = 0; i < n; i++) {
+                        new_arr[i] = arr[i];
+                   }
+                   
+                   delete[] arr; 
+
+                   arr = new_arr; 
+
+                   max = new_size; 
+		   
+	   }
 
     public:
         // miembros públicos, incluidos los heredados de List<T>
@@ -21,32 +36,42 @@ class ListArray : public List<T> {
 	    //Nuevos
 
 	    ListArray(){
-		   //
+		   arr = new T[MINSIZE]; //Reservar memoria
+                   max = MINSIZE; 
+                   n = 0; 
+
 
 	    } 
 
 	    ~ListArray(){
-		   //
+		   delete[] arr; 
 	 
  	    }
 
 
 	   T operator[] (int pos){
-		  //
-		  
-	   }
+                if(pos < 0 || pos >= n){
+                        throw out_of_range("Posición fuera de rango"); 
+
+                }
+
+                return arr[pos]; 
+           }
+	
 
 
-	   friend ostream& operator<<(std::ostream &out, const ListArray<T> &list){
-		   //
+	   friend ostream& operator<<(ostream &out, const ListArray<T> &list){
+                out << "["; 
+		     for (int i = 0; i < list.n; i++) {
+                        out << list.arr[i];
+                        if (i < list.n - 1) {
+                                out << ", ";
+                        }
+                }
+                out << "]";
+                return out;
+        }  
 
-	   }
-
-
-	   void resize(int new_size){
-		   //
-		   
-	   }
 
 
 	//heredados
