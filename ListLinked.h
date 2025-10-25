08db3dff954c00cc3821f0bc5b5e3cd0380e3872
Paula <pbarter@upv.edu.es>
2025-10-26 01:17:27 +0200

    Make file actualizado con ListLinked.h

--- ListLinked.h
+++ ListLinked.h
@@ -1,4 +1,5 @@
 #include <ostream>
+#include <stdexcept>
 #include "List.h"
 #include "Node.h"
 using namespace std; 
@@ -41,6 +42,85 @@ class ListLinked : public List<T> {
 
             return aux->data;
         }
+
+        //heredados
+
+        void insert(int pos, T e) override {
+            if (pos < 0 || pos > n) {
+                throw std::out_of_range("Fuera de rango1");
+            }
+
+            if (pos == 0) {
+                Node<T>* node = new Node<T>(e, first);
+                first = node;
+            } else {
+                Node<T>* prev = first;
+                for (int i = 0; i < pos - 1; i++) prev = prev->next;
+                Node<T>* node = new Node<T>(e, prev->next);
+                prev->next = node;
+            }
+            n++;
+        }
+
+        void prepend(T e) override {
+            insert(0, e);
+        }
+
+        void append(T e) override {
+            insert(n, e);
+        }
+
+        T remove(int pos) override {
+            if (pos < 0 || pos >= n) {
+                throw std::out_of_range("Fuera de rango2");
+            }
+
+            T value;
+            if (pos == 0) {
+                Node<T>* node = first;
+                value = node->data;
+                first = first->next;
+                delete node;
+            } else {
+                Node<T>* prev = first;
+                for (int i = 0; i < pos - 1; i++) prev = prev->next;
+                Node<T>* node = prev->next;
+                value = node->data;
+                prev->next = node->next;
+                delete node;
+            }
+
+            n--;
+            return value;
+        }
+
+        T get(int pos) override {
+            if (pos < 0 || pos >= n) {
+                throw std::out_of_range("Fuera de rango3");
+            }
+            Node<T>* cur = first;
+            for (int i = 0; i < pos; i++) cur = cur->next;
+            return cur->data;
+        }
+
+        int search(T e) override {
+            Node<T>* cur = first;
+            for (int i = 0; i < n; i++) {
+                if (cur->data == e) return i;
+                cur = cur->next;
+            }
+            return -1;
+        }
+
+        bool empty() override {
+            return n == 0;
+        }
+
+        int size() override {
+            return n;
+        }
+
+        //fin heredados
 
         friend ostream& operator<<(ostream &out, const ListLinked<T> &list){
             out << "[";
